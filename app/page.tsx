215392a130d697b23f9cfab2ee1594813d2345a2
Ong'owo <76827981+mwafreaker@users.noreply.github.com>
2024-01-04 15:21:22 +0300

    mobile first UI hero

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,19 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 w-screen">
-      <p className='flex flex-row justify-between w-full'>
-        <strong>Ong'owo</strong>
-        <ul className='flex flex-col'>
-          <li>about</li>
+    <main className="sm:flex min-h-screen sm:flex-col md:flex-row sm:w-screen items-center">
+
+      <div className='flex flex-col items-center justify-between sm:w-screen md:w-1/2 py-4 gap-12 relative'>
+        <section className='flex flex-col relative text-center gap-2 justify-center items-center min-h-72 px-10'>
+          <strong className='text-4xl'>Ong'owo Wicklif</strong>
+          <p className='text-xs'>Software Developer</p>
+        </section>
+        <ul className='flex flex-col justify-center sm:w-full gap-2'>
+          <li>About</li>
           <li>Projects</li>
-          <li>about</li>
+          <li>Contact</li>
         </ul>
-      </p>
+      </div>
     </main>
   )
 }
--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,18 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+  },
+  theme: {
+    extend: {
+      screens: {
+        'sm': { 'raw': '(min-width: 320px)' },
+        'md': { 'raw': '(min-width: 765px)' },
+        'lg': { 'raw': '(min-width: 1020px)' },
+        'xl': { 'raw': '(min-width: 1280px)' },
+        '2xl': { 'raw': '(min-width: 1536px)' },
+      }
+    }
   },
 }
