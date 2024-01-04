329b5524357f024c1d5d7d8fbf502617652465d4
Ong'owo <76827981+mwafreaker@users.noreply.github.com>
2024-01-04 12:28:49 +0300

    new nextjs commit

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 w-screen">
+      <p className='flex flex-row justify-between w-full'>
+        <strong>Ong'owo</strong>
+        <ul className='flex flex-col'>
+          <li>about</li>
+          <li>about</li>
+          <li>about</li>
+        </ul>
+      </p>
+    </main>
+  )
+}
